5652c90136803569737890b11f3ad7eac399b3ab
Mustafa Tayyar <mtayyar77@gmail.com>
2024-01-16 00:06:49 +0300

    car.jsx Created

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import './index.css';
 import App from './App';
 import store from './store/configureStore';
 import AdminPanel from './adminPanel/AdminPanel';
+import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <AdminPanel />
+    <BrowserRouter>
+      <AdminPanel />
+    </BrowserRouter>
   </Provider>
 );

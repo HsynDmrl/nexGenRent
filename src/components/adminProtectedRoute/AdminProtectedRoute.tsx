7c91579206ee2590e42712b2119459cb7aeec6b0
Hüseyin Demirel <hsyn23dmrl@gmail.com>
2024-02-27 00:21:59 +0300

    adminprotectedroute fixed some bugs...

--- src/components/adminProtectedRoute/AdminProtectedRoute.tsx
+++ src/components/adminProtectedRoute/AdminProtectedRoute.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAppSelector } from '../../store/configStore/useAppSelector';
+import { useAppDispatch } from '../../store/configStore/useAppDispatch';
+import { RootState } from '../../store/configStore/configureStore';
+import { getByEmail } from '../../store/user/userSlice';
 
 const AdminProtectedRoute: React.FC = () => {
-  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
-  const isAdmin = useAppSelector((state) => state.user.dataFromById?.role.toString() === 'ADMIN');
+  
+	const dispatch = useAppDispatch();
+  const isAuthenticated = useAppSelector((state:RootState) => state.auth.isAuthenticated);
+  const isAdmin = useAppSelector((state:RootState) => state.user.dataFromById?.role.toString() === 'ADMIN');
 
-  if (!isAuthenticated || !isAdmin) {
+  useEffect(() => {
+    dispatch(getByEmail());
+  }, [dispatch]);
+
+  if (!isAuthenticated || isAdmin) {
     return <Navigate to="/" replace />;
   }
 
